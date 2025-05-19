48cb4d8968fb2217a26a563356ab5aacd9051503
ohmydays <ooojjhn5@gmail.com>
2025-05-19 10:20:19 +0900

    feat: Edit NavBar color and Add Search component

--- team-eight-frontend/src/App.js
+++ team-eight-frontend/src/App.js
@@ -5,12 +5,15 @@ import { Provider } from "./components/ui/provider";
 import Mypage from "./pages/Mypage";
 import MypageEdit from "./pages/MypageEdit";
 import MypageSettings from "./pages/MypageSettings";
+import Search from "./components/Search";
+import Home from "./pages/Home";
 
 function App() {
   return (
     <Provider>
       <div className="App">
         <NavBar></NavBar>
+        <Search></Search>
         <Mypage></Mypage>
         <Footer></Footer>
       </div>
